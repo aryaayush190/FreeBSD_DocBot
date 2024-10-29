cd04aeb620b44cc7d41c311d8ff7db1839025944
aryaayush190 <98604869+aryaayush190@users.noreply.github.com>
2024-10-29 22:24:44 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,4 +1,4 @@
-!pip install -q cassio datasets langchain openai tiktoken  
-!pip install PyPDF2    
-!pip install -U langchain-community    
+!pip install -q cassio datasets langchain openai tiktoken
+!pip install PyPDF2
+!pip install -U langchain-community
 !pip install openai==0.28
